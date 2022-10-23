a53c4d2c437f3cd46d2ff23005406c38fe1c3af3
sakib-72 <112200299+sakib-72@users.noreply.github.com>
2022-10-23 23:56:57 +0600

    Time: 42 ms (65.37%), Space: 24 MB (87.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>v1;
-        int mult=1,mult2=1,x,l=nums.size(),c0=0,zeroIndex;
-        for(int i=0;i<l;i++)
+        int n=nums.size();
+        vector<int>v1(n);
+       v1[n-1]=1;
+        for(int i=n-2;i>=0;i--)
         {
-            mult=mult*nums[i];
-            if(nums[i]==0)
-            {
-                c0++;
-                zeroIndex=i;
-            }
+            v1[i]=nums[i+1]*v1[i+1];
         }
-        if(c0>1)
+        int lp=1;
+        for(int i=0;i<n;i++)
         {
-            for(int i=0;i<l;i++)
-            {
-                v1.push_back(0);
-            }
-            return v1;
-        }
-        else if(c0==1)
-        {
-            for(int i=0;i<l;i++)
-            {
-                if(nums[i]!=0)
-                {
-                    mult2=mult2*nums[i];
-                }
-            }
-            for(int i=0;i<l;i++)
-            {
-                if(i==zeroIndex)
-                {
-                    v1.push_back(mult2);
-                }
-                else
-                {
-                    v1.push_back(0);
-                }
-            }
-            return v1;
-        }
-        for(int i=0;i<l;i++)
-        {
-           x=mult*pow(nums[i],-1); 
-            v1.push_back(x);
+            v1[i]=v1[i]*lp;
+            lp=lp*nums[i];
         }
         return v1;
     }
